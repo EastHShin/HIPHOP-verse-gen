c5ec13be1d48ed442c548ca623ef474b91707b49
easthshin <xldk78@gmail.com>
2021-07-21 18:08:26 +0900

    fix the duplicated input error

--- static/index.js
+++ static/index.js
@@ -1,28 +1,34 @@
+let isRunning = false;
 function suggestion() {
-    var textarea = document.getElementById("context");
-    var context = textarea.value;
-    console.log(context);
-
-    var formdata = new FormData();
-    formdata.append("context", context);
-    fetch(
-        "/hiphop",
-        {
-            method: "POST",
-            body: formdata
+    if (isRunning == false) {
+        var textarea = document.getElementById("context");
+        var context = textarea.value;
+        if(context == ""){
+            alert("write your context!");
+            isRunning = false;
+            return;
         }
-    )
-        .then(response => response.text())
-        .then(result => {
-            console.log(result)
-            concat(result)
-        });
+        isRunning = true;
+        var formdata = new FormData();
+        formdata.append("context", context);
+        fetch(
+            "/hiphop",
+            {
+                method: "POST",
+                body: formdata
+            }
+        )
+            .then(response => response.text())
+            .then(result => {
+                console.log(result)
+                concat(result)
+            });
 
-
+    }
 }
 
 function concat(newText) {
     var context = document.getElementById("context");
-
     context.value = newText;
+    isRunning = false;
 }
